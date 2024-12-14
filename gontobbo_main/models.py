4f12a4ccfd2c694ee854470aaf3b08a9e52ce636
Ru1n00 <ruhul.amin.tnt@gmail.com>
2024-12-14 14:38:08 +0600

    Make username field in admin and receptionist model unique

--- gontobbo_main/models.py
+++ gontobbo_main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class BaseUser(models.Model):
-    username = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
