4d37faa49ded5a26f28cae190affd16a2c6ffb22
Ru1n00 <ruhul.amin.tnt@gmail.com>
2024-12-14 10:38:21 +0600

    Create BaseUser Model

--- gontobbo_main/models.py
+++ gontobbo_main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class BaseUser(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
