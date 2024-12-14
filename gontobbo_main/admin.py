605bc749348b32fbedce498ac2f19924566cca47
Ru1n00 <ruhul.amin.tnt@gmail.com>
2024-12-14 14:36:19 +0600

    Add the models to admin panel

--- gontobbo_main/admin.py
+++ gontobbo_main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+admin.site.register(models.Admin)
+admin.site.register(models.Receptionist)
+admin.site.register(models.Trip)
+admin.site.register(models.TripCategory)
+admin.site.register(models.SeatType)
+admin.site.register(models.Booking)
